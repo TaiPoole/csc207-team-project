61b9baadd76d09e87006b86ea43c5f04ff51d232
blobcode <lhelme123@gmail.com>
2025-11-17 16:41:06 -0500

    Implement Adding Permissions

--- src/main/java/Server/PermissionManager.java
+++ src/main/java/Server/PermissionManager.java
@@ -16,6 +16,14 @@ public class PermissionManager {
         return permissions.get(user);
     }
 
+    public void addPermission(User user, Permission permission) {
+        this.permissions.get(user).add(permission);
+    }
+
+    public void setPermissions(User user, ArrayList<Permission> permissions) {
+        this.permissions.put(user, permissions);
+    }
+
     public boolean userHasPermission(User user, Permission permission) {
         return permissions.get(user).contains(permission);
     }
