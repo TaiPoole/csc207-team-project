57a39ed278aa0c480a0b4f38987e1d8cbb75c8de
toryuki <Doetsfpsatizr@gmail.com>
2025-11-28 00:27:19 -0500

    zoo

--- src/main/java/common/RandomNameGenerator.java
+++ src/main/java/common/RandomNameGenerator.java
@@ -8,11 +8,37 @@ import java.util.Random;
 public class RandomNameGenerator {
 
     private static final String[] DEFAULT_ADJECTIVES = {
-            "Quiet", "Happy", "Sleepy", "Cosmic", "Shiny", "Brave", "Silly"
+            "Quiet", "Happy", "Sleepy", "Cosmic", "Shiny", "Brave", "Silly",
+            "Gentle", "Lucky", "Rapid", "Calm", "Mellow", "Tiny", "Swift",
+            "Clever", "Bold", "Fuzzy", "Golden", "Mystic", "Vivid", "Witty"
     };
 
+    // ok now we have a zoo here.
     private static final String[] DEFAULT_ANIMALS = {
-            "Panda", "Fox", "Otter", "Tiger", "Penguin", "Cat", "Dog"
+            "Antelope", "Ape", "Badger", "Bat", "Bear",
+            "Bee", "Beetle", "Bison", "Boar", "Buffalo",
+            "Bunny", "Camel", "Canary", "Cat", "Cheetah",
+            "Cobra", "Condor", "Cougar", "Coyote", "Crane",
+            "Crow", "Deer", "Dingo", "Dolphin", "Donkey",
+            "Dragonfly", "Duck", "Eagle", "Egret", "Elephant",
+            "Elk", "Falcon", "Ferret", "Finch", "Fish",
+            "Fox", "Frog", "Gazelle", "Gecko", "Giraffe",
+            "Goat", "Goose", "Gorilla", "Grasshopper", "Hawk",
+            "Hedgehog", "Heron", "Hippo", "Horse", "Husky",
+            "Ibis", "Iguana", "Jaguar", "Jay", "Jellyfish",
+            "Kangaroo", "Koala", "Koi", "Lemur", "Leopard",
+            "Lion", "Lizard", "Llama", "Lobster", "Lynx",
+            "Magpie", "Manatee", "Mantis", "Meerkat", "Mole",
+            "Moose", "Mouse", "Narwhal", "Ocelot", "Octopus",
+            "Ostrich", "Otter", "Owl", "Panda", "Panther",
+            "Parrot", "Peacock", "Pelican", "Penguin", "Pigeon",
+            "Porcupine", "Puma", "Quail", "Rabbit", "Raccoon",
+            "Ram", "Raven", "Reindeer", "Rhino", "Robin",
+            "Salamander", "Seal", "Shark", "Sheep", "Skunk",
+            "Sloth", "Snail", "Snake", "Sparrow", "Spider",
+            "Swan", "Tiger", "Toad", "Turtle", "Viper",
+            "Walrus", "Weasel", "Whale", "Wolf", "Wombat",
+            "Yak", "Zebra"
     };
 
     private Random random = new Random();
